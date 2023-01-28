0ff16b2c6eb236b58088f6e7ad633efbe8e14718
cesar-gamez <64483947+cesar-gamez@users.noreply.github.com>
2023-01-28 11:11:00 -0600

    Created env

---
+++ Diago/Diago/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Diago
+//
+//  Created by Cesar Gamez on 1/28/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
