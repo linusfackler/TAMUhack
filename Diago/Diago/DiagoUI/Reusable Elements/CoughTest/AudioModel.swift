1c2ac7c0222a88e88111fcee2aee481c645801d1
Linus Fackler <78768883+linusfackler@users.noreply.github.com>
2023-01-30 14:14:59 -0600

    merge frontend into main

---
+++ Diago/Diago/DiagoUI/Reusable Elements/CoughTest/AudioModel.swift
@@ -0,0 +1,166 @@
+//
+//  AudioModel.swift
+//  Diago
+//
+//  Created by Cesar Gamez on 1/28/23.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+import AVFoundation
+import FirebaseStorage
+
+class AudioRecorder: NSObject, ObservableObject {
+    
+    override init() {
+        super.init()
+        fetchRecording()
+    }
+    
+    let objectWillChange = PassthroughSubject<AudioRecorder, Never>()
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    var recordings = [Recording]()
+    
+    var recording = false {
+        didSet {
+            objectWillChange.send(self)
+        }
+    }
+    
+    func startRecording() {
+        let recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+        } catch {
+            print("Failed to set up recording session")
+        }
+        
+        let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let audioFilename = documentPath.appendingPathComponent("0000000001.m4a")
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.record()
+            
+            recording = true
+//            BufferView(duration: 0, diagnosis: sendQuery())
+        } catch {
+            print("Could not start recording")
+        }
+    }
+    
+    func stopRecording() {
+        audioRecorder.stop()
+        recording = false
+        
+        fetchRecording()
+    }
+    
+    //    func fetchRecording() {
+    //        recordings.removeAll()
+    //
+    //        let storage = Storage.storage()
+    //        let storageRef = storage.reference()
+    //
+    //        let clipsRef = storageRef.child("clips/clip1.m4a")
+    //
+    //        let fileManager = FileManager.default
+    //
+    //        let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    //        let directoryContents = try! fileManager.contentsOfDirectory(at: documentDirectory, includingPropertiesForKeys: nil)
+    //        for audio in directoryContents {
+    //            let recording = Recording(fileURL: audio, createdAt: getFileDate(for: audio))
+    //            do {
+    //                let localFile = try Data(contentsOf: audio as URL)
+    //                let uploadTask = clipsRef.putData(localFile, metadata: nil) { (metadata, error) in
+    //                    guard let metadata = metadata else {
+    //                        return
+    //                    }
+    //                    let size = metadata.size
+    //                }
+    //            } catch {
+    //                print("error occured")
+    //            }
+    //
+    //            recordings.append(recording)
+    //        }
+    //
+    //        print(recordings)
+    //        recordings.sort(by: { $0.createdAt.compare($1.createdAt) == .orderedAscending})
+    //
+    //        objectWillChange.send(self)
+    //    }
+    //
+    //    func deleteRecording(urlsToDelete: [URL]) {
+    //
+    //        for url in urlsToDelete {
+    //            print(url)
+    //            do {
+    //               try FileManager.default.removeItem(at: url)
+    //            } catch {
+    //                print("File could not be deleted!")
+    //            }
+    //        }
+    //
+    //        fetchRecording()
+    //    }
+    //}
+    
+    func fetchRecording() {
+        recordings.removeAll()
+        
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        
+        let clipsRef = storageRef.child("clips/clip1.m4a")
+        
+        let fileManager = FileManager.default
+        
+        let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let directoryContents = try! fileManager.contentsOfDirectory(at: documentDirectory, includingPropertiesForKeys: nil)
+        for audio in directoryContents {
+            let recording = Recording(fileURL: audio, createdAt: getFileDate(for: audio))
+            do {
+                let localFile = try Data(contentsOf: audio as URL)
+                let uploadTask = clipsRef.putData(localFile, metadata: nil) { (metadata, error) in
+                    guard let metadata = metadata else {
+                        return
+                    }
+                    let size = metadata.size
+                }
+            } catch {
+                print("error occured")
+            }
+            recordings.append(recording)
+            break // add this line to break the loop after the first file
+            
+        }
+    }
+    struct Recording {
+        let fileURL: URL
+        let createdAt: Date
+    }
+    
+    func getFileDate(for file: URL) -> Date {
+        if let attributes = try? FileManager.default.attributesOfItem(atPath: file.path) as [FileAttributeKey: Any],
+           let creationDate = attributes[FileAttributeKey.creationDate] as? Date {
+            return creationDate
+        } else {
+            return Date()
+        }
+    }
+}
+
+
---
+++ Diago/Diago/DiagoUI/Reusable Elements/CoughTest/CoughTestView.swift
@@ -0,0 +1,96 @@
+//
+//  CoughTestView.swift
+//  Diago
+//
+//  Created by Cesar Gamez on 1/28/23.
+//
+
+import SwiftUI
+import DSWaveformImage
+import AVKit
+
+struct CoughTestView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    @State var recording = false
+    @StateObject var audioRecorder: AudioRecorder = AudioRecorder()
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.ui.primary
+                    .edgesIgnoringSafeArea(.all)
+                VStack(spacing: UIScreen.main.bounds.width * 0.05) {
+                    VStack(spacing: UIScreen.main.bounds.width * 0.05) {
+                        BackNavButton()
+                            .onTapGesture {
+                                presentationMode.wrappedValue.dismiss()
+                            }
+                        TitleTextsView(focusText: "Record your cough", trailText: "Respiration Test")
+                    }.padding()
+                    Spacer()
+                    MicrophoneView(recording: $recording, audioRecorder: audioRecorder)
+                }
+            }.edgesIgnoringSafeArea(.bottom)
+        }.navigationBarHidden(true)
+    }
+}
+
+struct MicrophoneView: View {
+    @State var complete = false
+    @Binding var recording: Bool
+    @StateObject var audioRecorder: AudioRecorder
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width*0.99)
+                .foregroundColor(.white)
+                .cornerRadius(CornerRadius.Card.radius)
+            ZStack {
+                VStack(spacing: UIScreen.main.bounds.width*0.65) {
+                    Text("Tap to start recording")
+                        .font(Font.custom("Gilroy-Medium", size: Size.Font.Button.titleSize/1.5))
+                    NavigationLink(destination: BufferView(duration: 5, diagnosis: "")) {
+                        ButtonView(type: .primaryStrong, text: "Submit")
+                    }
+                }
+                ZStack {
+                    Circle()
+                        .frame(width: recording ? UIScreen.main.bounds.width*0.4 : UIScreen.main.bounds.width*0.294, height: recording ? UIScreen.main.bounds.width*0.4 : UIScreen.main.bounds.width*0.294)
+                        .foregroundColor(.black.opacity(0.4))
+                    Image("MicButtonOff")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: UIScreen.main.bounds.width*0.294, height: UIScreen.main.bounds.width*0.294)
+                        .onTapGesture {
+                            if !recording {
+                                audioRecorder.startRecording()
+                            } else {
+                                audioRecorder.stopRecording()
+                            }
+                            withAnimation {
+                                recording.toggle()
+                            }
+                        }
+                }.offset(y: -UIScreen.main.bounds.width*0.06)
+            }
+        }
+    }
+}
+
+struct BackNavButton: View {
+    var body: some View {
+        HStack {
+            ZStack {
+                Circle()
+                    .foregroundColor(Color.ui.primaryWeak)
+                    .frame(width: (UIScreen.main.bounds.width * 0.123), height: (UIScreen.main.bounds.width * 0.123))
+                Image("ChevronLeft")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: (UIScreen.main.bounds.width * 0.093), height: (UIScreen.main.bounds.width * 0.093))
+                
+            }
+            Spacer()
+        }
+    }
+}
